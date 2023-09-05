5d083dc4868135feb27b3e877335038df01d1f9b
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 00:45:21 +0300

    completed.

--- lib/config.py
+++ lib/config.py
@@ -1,4 +1,22 @@
 import sqlite3
 
-CONN = sqlite3.connect('music.db')
+# Connect to the database
+CONN = sqlite3.connect('db/music.db')
 CURSOR = CONN.cursor()
+
+# Update data using an SQL query
+update_query = """
+UPDATE your_table_name
+SET column_name = new_value
+WHERE some_condition;
+"""
+
+# Execute the update query
+CURSOR.execute(update_query)
+
+# Commit the changes to the database
+CONN.commit()
+
+# Close the cursor and the connection
+CURSOR.close()
+CONN.close()
